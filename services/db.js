c005735adbaff3cbe5b563b3a4651584d1b91ba5
blackhatjohnny <ogwurujohnson@gmail.com>
2018-10-26 12:05:37 +0100

    💯💯 Write Script For Database Connection

---
+++ services/db.js
@@ -0,0 +1,14 @@
+const pg = require('pg');
+
+const config = {
+  user: 'school_reg', //this is the db user credential
+  database: 'school_register',
+  password: 'school_reg',
+  port: 5432,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000,
+};
+
+const pool = new pg.Pool(config);
+
+module.exports = pool; //export pool to be accessible  from an where within the application
