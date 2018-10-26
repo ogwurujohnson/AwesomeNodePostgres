cc59610473c953b514522f9e5a3e19844516c65a
blackhatjohnny <ogwurujohnson@gmail.com>
2018-10-26 14:18:52 +0100

    ♣️♣️ Write Script to Create Table Student

--- services/db.js
+++ services/db.js
@@ -11,4 +11,42 @@ const config = {
 
 const pool = new pg.Pool(config);
 
-module.exports = pool; //export pool to be accessible  from an where within the application
+pool.on('connect', () => {
+  console.log('connected to the Database');
+});
+
+const createTables = () => {
+  const schoolTable = `CREATE TABLE IF NOT EXISTS
+      students(
+        id SERIAL PRIMARY KEY,
+        student_name VARCHAR(128) NOT NULL,
+        student_age INT NOT NULL,
+        student_class VARCHAR(128) NOT NULL,
+        parent_contact VARCHAR(128) NOT NULL,
+        admission_date VARCHAR(128) NOT NULL
+      )`;
+  pool.query(schoolTable)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+
+//export pool and createTables to be accessible  from an where within the application
+module.exports = {
+  createTables,
+  pool,
+};
+
+require('make-runnable');
+
